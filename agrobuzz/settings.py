ab82241d76f676919841defb668755671eaf079e
carlos cordoba <carloscba1@hotmail.com>
2017-01-28 20:19:09 -0300

    - db config

--- agrobuzz/settings.py
+++ agrobuzz/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'agrobuzz.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
+        'HOST': os.environ['DBHOST'],
         'PORT': '3306',
-        'NAME': 'bccba',
-        'USER': 'root',
-        'PASSWORD': '',
+        'NAME': os.environ['DB'],
+        'USER': os.environ['DBUSER'],
+        'PASSWORD': os.environ['DBPASSWORD'],
     }
 }
 
